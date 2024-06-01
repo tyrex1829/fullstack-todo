5bf240d02eb89a4a68462c735ee5366b2b929b54
Saksham Khare <sakshamkhare19@gmail.com>
2024-06-01 18:52:50 +0530

    wrong way of fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 import "./App.css";
+import { CreateTodo } from "./components/CreateTodo";
+import { Todos } from "./components/Todos";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
 
-  return <div>Hi there</div>;
+  fetch("http://localhost:3000/todos").then(async (res) => {
+    const json = await res.json();
+    setTodos(json.todos);
+  });
+
+  return (
+    <div>
+      {/* For creating a todo */}
+      <CreateTodo />
+      {/* For rendering a todo */}
+      <Todos todos={[]} />
+    </div>
+  );
 }
 
 export default App;
