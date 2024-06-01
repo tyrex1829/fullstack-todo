d21a175877feba7faa5782a2aa6d156752e1fd6b
Saksham Khare <sakshamkhare19@gmail.com>
2024-06-01 19:08:01 +0530

    cors in backend

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ const { todo } = require("./db");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 const port = 3000;
 
